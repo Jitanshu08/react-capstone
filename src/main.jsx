a357fe1d309deb1a57f0f278a832a7ad0a49214f
Jitanshu Shaw <jitanshu.shaw1@gmail.com>
2024-06-18 21:27:42 +0530

    setting up react and routing

---
+++ src/main.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home.jsx";
+import Info from "./pages/Info.jsx";
+import Browse from "./pages/Browse.jsx";
+import Selection from "./pages/MovieSelection.jsx";
+import NotFound from "./pages/404.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/info" element={<Info />} />
+        <Route path="/movieSelection" element={<Selection />} />
+        <Route path="/browse" element={<Browse />} />
+        <Route path="*" element={<NotFound/>}/>
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
+);
